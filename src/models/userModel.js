773ff40b9057a3fef21a71df514fe65f78ae1919
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-24 15:55:18 +0530

    removed unique property from userName

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
-      unique: true,
     },
     email: {
       type: String,
