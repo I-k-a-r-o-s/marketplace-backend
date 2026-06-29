80c63895ffd71588edf7337082790291f2a065a0
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-29 12:07:26 +0530

    route to check if the user is authorized

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/authController.js";
+import {
+  getAuthorizedUser,
+  signIn,
+  signUp,
+} from "../controllers/authController.js";
+import { protectedRoute } from "../middlweware/authMiddleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
 authRouter.post("/signin", signIn);
+authRouter.get("/validate", protectedRoute, getAuthorizedUser);
 
 export default authRouter;
