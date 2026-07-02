4b67223bc315aabdc082b79433f7eb6a29f3e501
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-07-02 15:05:44 +0530

    delete user route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateUserInfo } from "../controllers/userController.js";
+import { deleteUser, updateUserInfo } from "../controllers/userController.js";
 import { protectedRoute } from "../middlweware/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.patch("/:id", protectedRoute, updateUserInfo);
+userRouter.delete("/", protectedRoute, deleteUser);
 
 export default userRouter;
