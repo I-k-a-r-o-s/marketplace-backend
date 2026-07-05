b7828150490c6b46f6ad8a3e6a922519c3e2af3f
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-07-05 20:20:22 +0530

    multer integration

--- src/routes/listingRouter.js
+++ src/routes/listingRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { protectedRoute } from "../middlweware/authMiddleware.js";
+import upload from "../middlweware/multer.js";
 import { createListing } from "../controllers/listingController.js";
 
 const listingRouter = express.Router();
 
-listingRouter.post("/", protectedRoute, createListing);
+listingRouter.post(
+  "/",
+  protectedRoute,
+  upload.array("images", 6),
+  createListing,
+);
 
-export default listingRouter
+export default listingRouter;
