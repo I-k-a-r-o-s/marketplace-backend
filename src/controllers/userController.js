1b7bd8c5fadccb291f5570c40276708215a14101
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-07-06 16:22:28 +0530

    modify updateUserInfo to use Id from cookie. added getUserListings functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,31 +3,18 @@ import bcrypt from "bcryptjs";
 import validator from "validator";
 import UserModel from "../models/userModel.js";
 import { cookiesOptions } from "../utils/authTokenCookies.js";
+import ListingModel from "../models/listingModel.js";
 
 export const updateUserInfo = async (req, res) => {
   try {
-    const { id } = req.params;
     const { userName, email, password } = req.body;
-
-    if (!id) {
-      return res.status(400).json({
-        success: false,
-        message: "User ID is required!",
-      });
-    }
+    const id = req.user.id;
 
     const existingUser = await UserModel.findById(id);
     if (!existingUser) {
       return res.status(404).json({
         success: false,
         message: "User not found!",
-      });
-    }
-
-    if (req.user.id !== id) {
-      return res.status(401).json({
-        success: false,
-        message: "You can only update your own account!",
       });
     }
 
@@ -104,5 +91,27 @@ export const deleteUser = async (req, res) => {
     });
   } catch (error) {
     errorResponse(res, "deleteUser", error);
+  }
+};
+
+export const getUserListings = async (req, res) => {
+  try {
+    const listings = await ListingModel.find({ userRef: req.user.id }).sort({
+      createdAt: -1,
+    });
+    if (!listings) {
+      return res.status(404).json({
+        success: false,
+        message: "No listings found!",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Listings fetched successfully!",
+      listings,
+    });
+  } catch (error) {
+    errorResponse(res, "getUserListings", error);
   }
 };
